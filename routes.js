4ead981040ee2e324fb55e5b92230ddc00dd0701
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-01-06 18:46:02 -0600

    Updating directories api to exclude app

--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
 var path = require('path');
+var _ = require('lodash');
 
 module.exports = function (app) {
     app.get('/api/projects', function (req, res) {
         var directories = getDirectories('./www');
+        
+        _.remove(directories, function(directory){
+           return directory === 'app'; 
+        });
 
         res.send(directories);
     });
