421fffe380acb05e8b7516d5c781d2d4a6f55f28
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-01-06 18:24:26 -0600

    Shuffling some things around to set up node

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+// serve static files
+app.use(express.static('www'));
+
+// register routes
+// require('./routes')(app);
+
+var server = app.listen(1337);
