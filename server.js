6ccf8ef236dc43c645507f276fbb61a422d5d9f8
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-01-06 18:42:41 -0600

    Updating port config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 1337;
 
 // serve static files
 app.use(express.static('www'));
@@ -8,4 +9,4 @@ app.use(express.static('www'));
 // register routes
 require('./routes')(app);
 
-var server = app.listen(1337);
+var server = app.listen(port);
